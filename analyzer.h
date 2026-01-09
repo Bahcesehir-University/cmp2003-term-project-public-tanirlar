14c21bd9f04f43acbfcb6a18d165b98a7711d139
erenunal0701 <eren.unal1@bahcesehir.edu.tr>
2026-01-09 20:19:35 +0300

    Change ingest method to ingestFile for TripAnalyzer
    Updated TripAnalyzer to ingest from a file instead of stdin.

--- analyzer.h
+++ analyzer.h
@@ -18,9 +18,10 @@ struct SlotCount {
 
 class TripAnalyzer {
 public:
-    void ingestStdin();
+    void ingestFile(const string& csvPath);
     std::vector<ZoneCount> topZones(int k = 10) const;
     std::vector<SlotCount> topBusySlots(int k = 10) const;
     std::unordered_map<std::string, long long> zoneMapCount;
     std::unordered_map<std::string, array<long long, 24>> slotMapCount;
 };
+
